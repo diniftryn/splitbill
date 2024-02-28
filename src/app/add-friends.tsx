5ef5f95d06332c7a90f787cb1a67fcd140ffa3a5
Dini <dini@Dinis-MacBook-Air.local>
2024-02-29 04:11:11 +0800

    successfully add contacts

--- src/app/add-friends.tsx
+++ src/app/add-friends.tsx
@@ -1,10 +1,29 @@
 import { View, Text, TouchableOpacity, ScrollView, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import * as Contacts from "expo-contacts";
 
 export default function AddFriendsScreen() {
-  const contacts = ["Ahn Ye-won", "An Min-young", "Cha Hyun-seung", "Cho Min-ji", "Choi Hye-seon", "Choi Jong-woo", "Choi Min-woo", "Choi Seo-eun", "Choi Si-hun", "Jo Yoong-jae", "Kang So-yeon", "Kim Gyu-ri", "Kim Han-bin", "Kim Hyeon-joong", "Kim Jin-young", "Kim Jun-sik", "Kim Se-jun", "Kim Su-min", "Lee Gwan-hee", "Lee Jin-seok", "Lee Nadine", "Lee So-e", "Lim Min-su", "Moon Se-hoon", "Oh Jin-taek", "Park Min-kyu", "Park Se-jeong", "Seong Min-ji", "Shin Dong-woo", "Shin Ji-yeon", "Shin Seul-ki", "Son Won Ik", "Song Ji-a", "Yu Si-eun", "Yun Ha-bin", "Yun Ha-jeong"];
+  // const contacts = ["Ahn Ye-won", "An Min-young", "Cha Hyun-seung", "Cho Min-ji", "Choi Hye-seon", "Choi Jong-woo", "Choi Min-woo", "Choi Seo-eun", "Choi Si-hun", "Jo Yoong-jae", "Kang So-yeon", "Kim Gyu-ri", "Kim Han-bin", "Kim Hyeon-joong", "Kim Jin-young", "Kim Jun-sik", "Kim Se-jun", "Kim Su-min", "Lee Gwan-hee", "Lee Jin-seok", "Lee Nadine", "Lee So-e", "Lim Min-su", "Moon Se-hoon", "Oh Jin-taek", "Park Min-kyu", "Park Se-jeong", "Seong Min-ji", "Shin Dong-woo", "Shin Ji-yeon", "Shin Seul-ki", "Son Won Ik", "Song Ji-a", "Yu Si-eun", "Yun Ha-bin", "Yun Ha-jeong"];
+  const [contacts, setContacts] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === "granted") {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails]
+        });
+
+        if (data.length > 0) {
+          setContacts(data);
+          console.log(contacts);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <SafeAreaView className="mb-24">
       <Stack.Screen
@@ -23,10 +42,8 @@ export default function AddFriendsScreen() {
         <Text className="pl-1">search friends from contacts list</Text>
       </View>
       <ScrollView className="px-3">
-        {contacts.length > 0 &&
-          contacts.map(contact => {
-            return <Text className="text-base py-2">{contact}</Text>;
-          })}
+        {/* <Text>{JSON.stringify(contacts)}</Text> */}
+        {contacts && contacts.map(contact => <Text className="text-base py-2">{contact.name}</Text>)}
       </ScrollView>
     </SafeAreaView>
   );
