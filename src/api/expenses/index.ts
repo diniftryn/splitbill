dedec01fd1666d65cd8e4bb62778a270df31dca2
Dini <dini@Dinis-MacBook-Air.local>
2024-03-02 21:35:30 +0800

    added query for group expenses

--- src/api/expenses/index.ts
+++ src/api/expenses/index.ts
@@ -1,9 +1,27 @@
 import { supabase } from "@/src/lib/supabase";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useFriendGroup } from "../friends";
 
-export const useExpensesList = (groupId: string) => {
+export const useFriendExpensesList = (userId: string, friendId: string) => {
   return useQuery({
-    queryKey: ["expenses"],
+    queryKey: ["friendExpenses"],
+    queryFn: async () => {
+      const { data: friendGroup, error: errorFriendGroup } = await supabase.from("groups").select("*").contains("userIds", [userId, friendId]).eq("type", "friend");
+      if (errorFriendGroup) {
+        throw new Error(errorFriendGroup.message);
+      }
+      const { data, error } = await supabase.from("expenses").select("*").eq("groupId", friendGroup[0].id);
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    }
+  });
+};
+
+export const useGroupExpensesList = (groupId: string) => {
+  return useQuery({
+    queryKey: ["groupExpenses"],
     queryFn: async () => {
       const { data, error } = await supabase.from("expenses").select("*").eq("groupId", groupId);
       if (error) {
--- src/app/(tabs)/groups/[name].tsx
+++ src/app/(tabs)/groups/[name].tsx
@@ -1,21 +1,21 @@
 import { Text } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import Expenses from "@/src/components/Expenses";
-import { useExpensesList } from "@/src/api/expenses";
+import { useGroupExpensesList } from "@/src/api/expenses";
 import { ActivityIndicator } from "react-native";
 
 export default function GroupsExpensesScreen() {
   const { name, groupId } = useLocalSearchParams();
 
-  const { data: expenses, error, isLoading } = useExpensesList(groupId as string);
+  const { data: groupExpenses, error, isLoading } = useGroupExpensesList(groupId as string);
 
   if (isLoading) {
     return <ActivityIndicator />;
   }
 
   if (error) {
-    return <Text>Failed to fetch groups</Text>;
+    return <Text>Failed to fetch group expenses</Text>;
   }
 
-  return <Expenses expenses={expenses as Expense[]} name={name as string} path="groups" />;
+  return <Expenses expenses={groupExpenses as Expense[]} name={name as string} path="groups" />;
 }
