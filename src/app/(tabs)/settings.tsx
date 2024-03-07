7e343a95efe561ce7a7b2b6ee4b32fa7eb4f28bb
Dini <dini@Dinis-MacBook-Air.local>
2024-03-08 00:01:41 +0800

    modified settings, removed split-details in add-expense, updated handling null in add group

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -2,8 +2,11 @@ import { View, Text, TextInput, Image, TouchableOpacity, Button } from "react-na
 import { Stack } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
 import { supabase } from "@/src/lib/supabase";
+import { useAuth } from "@/src/providers/AuthProvider";
 
 export default function SettingsScreen() {
+  const { user } = useAuth();
+
   return (
     <View className="flex-1 justify-center items-center bg-rose-50">
       <Stack.Screen
@@ -23,9 +26,8 @@ export default function SettingsScreen() {
         <View className="w-full border border-t-[0.5px] rounded-b-3xl py-5 flex justify-center items-center bg-white">
           <Image source={require("@/assets/images/clouds1.jpeg")} className="w-[90px] h-[90px] border rounded-full mb-5" />
           <View className="bg-pink-200 w-3/4 flex items-center pb-3 mb-3">
-            <TextInput className="text-lg font-medium pb-2" value="Dini F" />
-            <TextInput className=" font-normal" value="9999 1234" />
-            <TextInput className="font-normal" value="name@email.com" />
+            <TextInput className="text-lg font-medium pb-2" value={user?.username} />
+            <TextInput className=" font-normal" value={user?.phone} />
           </View>
 
           <Button title="Logout" onPress={async () => await supabase.auth.signOut()} />
